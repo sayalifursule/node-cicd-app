45a39ac698579de0ca1affcba5c67c72b9c5d29e
sayalifursule <147057108+sayalifursule@users.noreply.github.com>
2026-04-26 23:09:08 +0530

    Update server response message
    Removed emoji from response message.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from CI/CD 🚀");
+  res.end("Hello from CI/CD");
 });
 
 server.listen(3000, () => {
